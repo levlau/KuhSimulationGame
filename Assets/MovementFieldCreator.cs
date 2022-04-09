4eb768e02716da2346a8638f3fdb3c282d7665dd
levi <levlau@web.de>
2022-04-09 15:39:58 +0200

    mit kuh movement angefangen

--- Assets/MovementFieldCreator.cs
+++ Assets/MovementFieldCreator.cs
@@ -9,19 +9,20 @@ public class MovementFieldCreator : MonoBehaviour
     [SerializeField] int lengthCount;
     [SerializeField] float yOffset;
 
+
     private void Start()
     {
-        float offset = transform.localScale.x - 2 * fencePadding / lengthCount;
+        float minXPos = transform.localScale.x / 2f;
+        float minZPos = transform.localScale.z / 2f;
 
-        float xScale = transform.localScale.x * 2f;
-        float zScale = transform.localScale.z * 2f;
+        float offset = (transform.localScale.x - fencePadding) / lengthCount;
 
-        Vector3 pos = new Vector3(-xScale + fencePadding, transform.position.y + yOffset, -zScale + fencePadding);
+        Vector3 pos = new Vector3(-minXPos + fencePadding, transform.position.y + yOffset, -minZPos + fencePadding);
         float startZPos = pos.z;
 
-        for(int x = 0; x < lengthCount; x++)
+        for (int x = 0; x < lengthCount; x++)
         {
-            for(int z = 0; z < lengthCount; z++)
+            for (int z = 0; z < lengthCount; z++)
             {
                 Instantiate(cube, pos, transform.rotation);
                 pos.z += offset;
@@ -30,4 +31,5 @@ public class MovementFieldCreator : MonoBehaviour
             pos.x += offset;
         }
     }
+
 }
